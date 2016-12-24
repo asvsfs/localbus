0270c0eb1e52fef50ff988e0770041a222339818
Amin Taghaddosi <palizesoftware@gmail.com>
2016-12-24 17:01:15 +0330

    سبد خريد انبارداري ثبت محصول جديد سفارش و ...

--- app/src/main/java/amir/app/business/management/test.java
+++
@@ -1,8 +0,0 @@
-package amir.app.business.management;
-
-/**
- * Created by amin on 11/20/2016.
- */
-
-public class test {
-}
--- app/src/main/java/amir/app/business/models/Location.java
+++
@@ -1,8 +0,0 @@
-package amir.app.business.models;
-
-import com.strongloop.android.loopback.Model;
-
-public class Location extends Model {
-    public float lat;
-    public float lng;
-}
